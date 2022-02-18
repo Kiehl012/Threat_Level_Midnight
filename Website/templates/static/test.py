c3e262398034efca2796bebd916145c0e1bf261f
Kayleigh Bradley-Vogt <kbradleyvogt@gmail.com>
2022-02-17 20:49:15 -0600

    update

---
+++ Website/templates/static/test.py
@@ -0,0 +1,19 @@
+f
+from flask import Flask,render_template,request
+ 
+app = Flask(__name__)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('index.html',form_data = form_data)
+ 
+ 
+app.run(host='localhost', port=5000)
